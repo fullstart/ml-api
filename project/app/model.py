5758927c2c3d9f6d72651c70134d84a89aa10770
fullstart <42534645+fullstart@users.noreply.github.com>
2021-12-10 19:51:26 +0300

    Switched to predict_proba, added successful test

--- project/app/model.py
+++ project/app/model.py
@@ -5,10 +5,8 @@ Prediction Model definition and helper functions.
 @author: Andrey Aksenov
 """
 
-import joblib
 import pandas as pd
-from sklearn.svm import SVC
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import dill
 
 # define data structures
@@ -24,19 +22,19 @@ class HeartInfo(BaseModel):
 
     """
 
-    age:      int
-    sex:      int
-    cp:       int
-    trestbps: int
-    chol:     int
-    fbs:      int
-    restecg:  int
-    thalach:  int
-    exang:    int
-    oldpeak:  float
-    slope:    int
-    ca:       int
-    thal:     int
+    age:      int = Field(ge=20, le=150)
+    sex:      int = Field(ge=0, le=1)
+    cp:       int = Field(ge=0, le=3)
+    trestbps: int = Field(ge=80, le=200)
+    chol:     int = Field(ge=100, le=600)
+    fbs:      int = Field(ge=0, le=1)
+    restecg:  int = Field(ge=0, le=2)
+    thalach:  int = Field(ge=60, le=250)
+    exang:    int = Field(ge=0, le=1)
+    oldpeak:  float = Field(ge=0, le=7)
+    slope:    int = Field(ge=0, le=2)
+    ca:       int = Field(ge=0, le=4)
+    thal:     int = Field(ge=0, le=3)
 
 
 class HeartPredict(BaseModel):
@@ -67,7 +65,7 @@ def predict(heart_data: HeartInfo):
         model = dill.load(model_file)
     
     data = pd.DataFrame(heart_data.dict(), index=[0]).drop(["fbs"], axis=1)
-    prediction = model.predict(data)
-    ret_val = {"prediction": prediction}
+    prediction = model.predict_proba(data)
+    ret_val = {"prediction": prediction[0][1]}
 
     return HeartPredict(**ret_val)
